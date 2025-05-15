f3b7d01ed1093ccde15536975a6caeccd7356746
Haron M Maina <haronmmaina@Harons-MacBook-Air.local>
2025-05-15 18:57:55 +0300

    Adds changes

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,14 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        # Initialize base User with first and last name
+        super().__init__(first_name, last_name)
+        # Each student starts with an empty knowledge list
+        self.knowledge = []
+
+    def learn(self, fact):
+        """
+        Add a new fact (string) to the student's knowledge list.
+        """
+        self.knowledge.append(fact)
